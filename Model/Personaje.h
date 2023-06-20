129fa025c574b850b052736286ab1b17e0c23390
Pickle <alonsochullunquia@gmail.com>
2023-06-20 09:55:17 -0500

    feat: Entidad (set's, constructor), Personaje(atrib, virtual fun), Jugador(atrib, set's, get's, void mover)

---
+++ Model/Personaje.h
@@ -0,0 +1,19 @@
+//
+// Created by pickle on 20/06/23.
+//
+
+#ifndef REVOLUTION_GAME_PERSONAJE_H
+#define REVOLUTION_GAME_PERSONAJE_H
+#include "Entidad.h"
+class Personaje : public Entidad {
+protected:
+    unsigned int vida;
+    unsigned int rapidez;
+    vector<int> velocidad;
+    //Arma* arma = Nullptr;
+public:
+    virtual void mover()=0;
+    virtual void actualizar()=0;
+};
+
+#endif //REVOLUTION_GAME_PERSONAJE_H
