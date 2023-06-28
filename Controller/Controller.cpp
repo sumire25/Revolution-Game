cfd79c2f942a719c9ba17863c23ca4058fd50230
Erick <itzlilouxx@gmail.com>
2023-06-28 11:08:49 -0500

    Se ha comentado el evento del teclado en el Poll Event

--- Controller/Controller.cpp
+++ Controller/Controller.cpp
@@ -18,25 +18,6 @@ namespace Ctlr {
           case sf::Event::Closed:
             vista.getCloseWindow();
             break;
-            /*case sf::Event::KeyPressed:
-              if (vista.getEvent().key.code == sf::Keyboard::W) {
-                y -= 10;
-                vista.drawEsfera(50, (x - 100) * (-1), (y - 100) * (-1));
-                std::cout << "Tecla W presionada" << std::endl;
-              } else if (vista.getEvent().key.code == sf::Keyboard::A) {
-                x -= 10;
-                vista.drawEsfera(50, (x - 100) * (-1), (y - 100) * (-1));
-                std::cout << "Tecla A presionada" << std::endl;
-              } else if (vista.getEvent().key.code == sf::Keyboard::S) {
-                y += 10;
-                vista.drawEsfera(50, (x - 100) * (-1), (y - 100) * (-1));
-                std::cout << "Tecla S presionada" << std::endl;
-              } else if (vista.getEvent().key.code == sf::Keyboard::D) {
-                x += 10;
-                vista.drawEsfera(50, (x - 100) * (-1), (y - 100) * (-1));
-                std::cout << "Tecla D presionada" << std::endl;
-              }
-              break;*/
         }
       }
       if (vista.getKeyboard_W()) {
