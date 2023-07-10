539326898de2a2af54d592743597b607a7b2aacf
Erick <itzlilouxx@gmail.com>
2023-07-10 14:22:59 -0500

    Se esta resolviendo bugs en el menu

--- main.cpp
+++ main.cpp
@@ -1,16 +1,18 @@
 #include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
 #include <iostream>
 #include "Model/Model.h"
 #include "View/View.h"
 #include "Controller/Controller.h"
 
 int main() {
-    Mdl::Model modelo;
-    modelo.myJuego.p1->mostrar();
-    modelo.myJuego.p1->setDerecha(true);
-    modelo.actualizar();
-    modelo.myJuego.p1->mostrar();
-    Ctlr::Controller controlador;
-    controlador.ejecutar();
-    return 0;
+
+  Mdl::Model modelo;
+  modelo.myJuego.p1->mostrar();
+  modelo.myJuego.p1->setDerecha(true);
+  modelo.actualizar();
+  modelo.myJuego.p1->mostrar();
+  Ctlr::Controller controlador;
+  controlador.ejecutar();
+  return 0;
 }
